a2eee53925fe69fdf3555cc305d840f88c5729be
Ben Fiedler <git@bfiedler.ch>
2021-09-10 13:53:54 +0200

    fmt: Add overwriting mode
    Heavily inspired by gofmt.go

--- fmt.go
+++ fmt.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
-	"log"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"regexp"
 
 	"github.com/spf13/pflag"
@@ -25,30 +27,76 @@ func runFmt() {
 	pflag.Parse()
 
 	if pflag.NArg() == 0 {
-		format(os.Stdin, os.Stdout)
+		processFile("<standard input>", os.Stdin, os.Stdout)
 	} else {
-		if *overwriteFiles {
-			log.Fatalf("not implemented yet")
-		} else {
-			for _, file := range pflag.Args() {
-				r, err := os.Open(file)
-				if err != nil {
-					log.Fatalf("error opening file %s: %v", file, err)
-				}
-
-				format(r, os.Stdout)
-			}
+		for _, file := range pflag.Args() {
+			processFile(file, nil, os.Stdout)
 		}
 	}
 }
 
-func format(in io.Reader, out io.Writer) error {
-	s := bufio.NewScanner(in)
+func processFile(filename string, in io.Reader, out io.Writer) error {
+	var perm fs.FileMode = 0644
+	if in == nil {
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		fi, err := f.Stat()
+		if err != nil {
+			return err
+		}
+		in = f
+		perm = fi.Mode().Perm()
+	}
 
-	var err error
+	content, err := io.ReadAll(in)
+	if err != nil {
+		return err
+	}
+
+	buf, err := format(content)
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(buf, content) {
+		if *overwriteFiles {
+			backup, err := backupFile(filename+".", content, perm)
+			if err != nil {
+				return err
+			}
+
+			err = os.WriteFile(filename, buf, perm)
+			if err != nil {
+				os.Rename(backup, filename)
+				return err
+			}
+
+			err = os.Remove(backup)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if !*overwriteFiles {
+		_, err = out.Write(buf)
+	}
+
+	return err
+}
+
+func format(in []byte) ([]byte, error) {
+	s := bufio.NewScanner(bytes.NewBuffer(in))
+	var b bytes.Buffer
+	out := bufio.NewWriter(&b)
+
 	for s.Scan() {
 		line := s.Text()
 
+		var err error
 		if match := commentStart.FindStringSubmatch(line); match != nil {
 			_, err = fmt.Fprintln(out, line)
 		} else if match := hasAmount.FindStringSubmatch(line); match != nil {
@@ -65,9 +113,42 @@ func format(in io.Reader, out io.Writer) error {
 		}
 
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	return s.Err()
+	err := s.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	err = out.Flush()
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func backupFile(name string, content []byte, perm fs.FileMode) (string, error) {
+	f, err := os.CreateTemp(filepath.Dir(name), filepath.Base(name))
+	if err != nil {
+		return "", err
+	}
+
+	backup := f.Name()
+
+	err = f.Chmod(perm)
+	if err != nil {
+		f.Close()
+		os.Remove(backup)
+		return backup, err
+	}
+
+	_, err = f.Write(content)
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+
+	return backup, err
 }
